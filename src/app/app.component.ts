dd28f8dfa7cf9a1d35e901c75ea3a62f7aab1784
maxamed-damie <m.daymie@outlook.com>
2022-04-11 17:30:05 +0200

    password generator commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwgenerator';
+  passwordLength: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  password: string = '';
+
+  onButtonClick(){
+    const numbers: string = '123456789';
+    const letters: string = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols: string = '!"#€%&/()';
+
+    let combinedPassword: string = '';
+
+    if (this.includeLetters || this.includeNumbers || this.includeSymbols && this.passwordLength > 0) {
+      if (this.includeLetters)
+        combinedPassword += letters;
+
+      if (this.includeNumbers)
+        combinedPassword += numbers;
+
+      if (this.includeSymbols)
+        combinedPassword += symbols;
+
+      let generatedPassword: string = '';
+      for (let i = 0; i < this.passwordLength; i++) {
+        const index = Math.floor(Math.random() * combinedPassword.length);
+        generatedPassword += combinedPassword[index];
+      }
+      this.password = generatedPassword;
+    }
+  }
+
 }
